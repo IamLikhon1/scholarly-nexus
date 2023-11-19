50ace23e6ca1cefc957c0b981c3092b46317ee8a
IamLikhon1 <likhonadnan55@gmail.com>
2023-11-19 11:52:06 +0600

    setup

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+import Footer from "@/Components/HomePage/Footer/Footer";
+import Navbar from "@/Components/HomePage/Navbar/Navbar";
+
+export default function Home() {
+
+  return (
+    <main>
+      <Navbar />
+      <Footer />
+    </main>
+  )
+}
